d354b01f336d1bb9d68442d6f0d80549e28d6bd5
USER <USER@DESKTOP-TRQG01D>
2022-09-30 18:50:10 +0300

    Добавьте файлы проекта.

---
+++ Pr5.Dop.3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Tom"; int age = 34; //Присвоение строкового типа "string" лок.переменной "name"; присвоение целого типа "int" лок.переменной "age".
+            double height = 1.7; //присвоение вещественного типа "double" лок.переменной "height".
+            Console.WriteLine($"Имя: {name} Возраст: {age} Рост: {height}м"); // вывод на экран имени, возраста и роста с присвоенными им выше значениями.
+            Console.ReadKey(); // ожидание нажатия пользователем на любую клавишу для закрытия консоли.
+        }
+    }
+}
